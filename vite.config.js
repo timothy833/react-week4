52d1b0d363348f867e7dc0fabc1b13ad1e1dfea7
timothy833 <ca9m8e5zy@gmail.com>
2025-01-31 23:10:09 +0800

    week4 update2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({mode})=>{
   return {
-    base: mode === 'production' ? 'react-week4': '/',
+    base: mode === 'production' ? '/react-week4/': '/',
     plugins: [react()],
   }
 })
